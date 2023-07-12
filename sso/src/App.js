1f37a64adcd3166349ca110a9281eb92bd6ab36a
Deshan Harshana <nawarathnadeshan@gmail.com>
2023-07-12 09:01:54 +0530

    first commit

---
+++ sso/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Login from "./pages/Login";
+
+
+const App = () => {
+
+  return (
+    
+      <div>
+        <Navbar />
+        <Login />
+        </div>
+   
+  );
+};
+
+export default App;
---
+++ sso/src/components/Navbar.js
@@ -0,0 +1,26 @@
+
+const Navbar = () => {
+ 
+  return (
+    <div className="navbar">
+      <span className="logo">
+          Lama App
+      </span>
+        <ul className="list">
+          <li className="listItem">
+            <img
+              src="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt=""
+              className="avatar"
+            />
+          </li>
+          <li className="listItem">Deshan</li>
+          <li className="listItem"> Logout </li>
+        </ul>
+        </div>
+      
+    
+  );
+};
+
+export default Navbar;
